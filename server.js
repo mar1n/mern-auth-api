4d387417831a9e90fcea0b58e830e6e9547079da
mar1n <30738216+mar1n@users.noreply.github.com>
2020-10-10 22:15:30 +0100

    connect to mongodb

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ require('dotenv').config();
 
 const app = express();
 
+// connect to db
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log('DB CONNECTION ERROR: ', err));
+
 //import routes
 const authRoutes = require('./routes/auth');
 const { body } = require('express-validator');
@@ -24,7 +35,7 @@ if(process.env.NODE_ENV = 'development') {
 // middleware
 app.use('/api', authRoutes);
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`API is running on port ${port}`);
 })
