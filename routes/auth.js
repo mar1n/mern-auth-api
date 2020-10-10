93db51e05283ab37f82f0d1a923e536e8a486c81
mar1n <30738216+mar1n@users.noreply.github.com>
2020-10-10 21:47:16 +0100

    moving routes

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/signup', (req, res) => {
+    res.json({
+        data: 'you hit signup endpoint sss'
+    });
+});
+
+module.exports = router;
