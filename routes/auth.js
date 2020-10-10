24ee13e8c46f61cfff54bcccd0a5b2c1e07c0f08
mar1n <30738216+mar1n@users.noreply.github.com>
2020-10-10 23:19:49 +0100

    express validator

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 const { signup } = require('../controllers/auth');
 
-router.get('/signup', signup);
+// import validators
+const { userSignupValidator } = require('../validators/auth');
+const { runValidation } = require('../validators');
+
+router.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = router;
