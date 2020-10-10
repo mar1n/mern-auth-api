61526bbb8573e65a5aedc150ea7960bb9c57cf17
mar1n <30738216+mar1n@users.noreply.github.com>
2020-10-10 22:03:33 +0100

    applying middlewares

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
 
 //import routes
 const authRoutes = require('./routes/auth');
+const { body } = require('express-validator');
+
+// app middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//app.use(cors()); // allows all origins
+if(process.env.NODE_ENV = 'development') {
+    app.use(cors({origin: `http://localhost:3000`}))
+}
 
 // middleware
 app.use('/api', authRoutes);
